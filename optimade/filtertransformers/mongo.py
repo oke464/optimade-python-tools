bb67af940ef23624813f4c9282f0757180653329
Matthew Evans <git@ml-evs.science>
2020-11-19 19:12:04 +0000

    Strip tzinfo from incoming date

--- optimade/filtertransformers/mongo.py
+++ optimade/filtertransformers/mongo.py
@@ -496,7 +496,7 @@ class MongoTransformer(Transformer):
             for operator in subdict[prop]:
                 subdict[prop][operator] = bson.json_util.loads(
                     bson.json_util.dumps({"$date": subdict[prop][operator]})
-                )
+                ).replace(tzinfo=None)
 
             return subdict
 
